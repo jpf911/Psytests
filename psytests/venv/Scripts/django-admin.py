a757e0414293b98f51c8839daa5e69e2e705fa93
Jerwin Carreon <jerwin.carreon@jmc.edu.ph>
2021-10-15 12:42:09 +0800

    update

--- psytests/venv/Scripts/django-admin.py
+++ psytests/venv/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!c:\users\jerwi\documents\python\psytests\psytests\venv\scripts\python.exe
+#!C:\Users\jerwi\Documents\Capstone\Psytests\psytests\venv\Scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
