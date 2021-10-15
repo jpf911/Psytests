b8aa2b7481f2e597de5f790b8f68091e6bc681e7
jpf911 <japhet.floren@jmc.edu.ph>
2021-10-15 10:25:11 +0800

    added result.html

--- psytests/riasec/views.py
+++ psytests/riasec/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from .models import RIASEC_Test
 # Create your views here.
 
 def testPage(request):
-    return render(request,'riasec/test.html')
+    questions=RIASEC_Test.objects.all()
+    return render(request,'riasec/test.html', {"questions":questions})
+
+def add(request):
+    r= float(request.GET.get('choices'))
+    return render(request,'riasec/result.html',{'result':r})
+
 
 def home(request):
     return render(request,'riasec/riasec_home.html')
